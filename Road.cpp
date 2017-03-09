8e02e80be72f7a5217a740c88aca8324259c10ce
patricknugrohoh <patrick.nugroho64@yahoo.com>
2017-03-09 19:12:19 +0700

    Ini road.h dan cpp nya

---
+++ Road.cpp
@@ -0,0 +1,9 @@
+#include "Road.h"
+	
+	Road::Road(int x,int y):Cell(x,y){
+		
+	}
+		
+	void Road::render(){
+		cout<<" ";
+	}
--- Road.h
+++ Road.h
@@ -2,8 +2,22 @@
 #define ROAD_H
 #include "Facility.h"
 
+/** @class Road
+  * Real class
+  */
 class Road: public Facility{
-	
-};
 
+	
+	/** @brief Constructor.
+	  * Konstruktor kelas Road.
+	  */  
+	/** @param x Parameter x untuk loc.
+	  */    
+	/** @param y Parameter y untuk loc.
+	  */ 
+	Road(int x,int y);
+	
+
+	void render();
+};
 #endif
