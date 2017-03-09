ca032dacb5de00f0a38152cd023adcf7f123d861
rumahkumana <suka_berkhayal@yahoo.co.id>
2017-03-09 19:24:25 +0700

    gotoxy, point

--- Point.cpp
+++ Point.cpp
@@ -1,2 +1,10 @@
 #include "Point.h"
+#include <iostream>
+using namespace std;
 
+Point :: Point(int x, int y) {setX(x); setY(y);}
+int Point::getX() {return x;}
+int Point::getY() {return y;}
+void Point::setX(int x) {this->x = x;}
+void Point::setY(int y) {this->y = y;}
+void Point::add(int x, int y) {this->x += x; this->y += y;}
