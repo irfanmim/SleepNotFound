3637fde1b19bb3eeea12b4d31ab2b9eda9d72a50
patricknugrohoh <patrick.nugroho64@yahoo.com>
2017-03-11 15:39:45 +0700

    ini yg mammal2 dll

---
+++ Taxonomy.cpp
@@ -0,0 +1,9 @@
+#include "Taxonomy.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+Taxonomy::Taxonomy(string tax_name):taxName(tax_name){
+	
+}
+  
--- Taxonomy.h
+++ Taxonomy.h
@@ -1,8 +1,11 @@
 #ifndef TAXONOMY_H
 #define TAXONOMY_H
+#include <iostream>
+using namespace std;
 
 class Taxonomy{
 	public:
+		Taxonomy(string tax_name);
 		virtual void showTaxName()=0;
 	protected:
 		const string taxName;
