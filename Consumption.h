d40cfadafff32c46f9d2c72e26c521b752bd8bd7
patricknugrohoh <patrick.nugroho64@yahoo.com>
2017-03-11 22:36:11 +0700

    consumption.h

--- Consumption.h
+++ Consumption.h
@@ -1,39 +1,70 @@
 #ifndef CONSUMPTION_H
 #define CONSUMPTION_H
 #define factor 0.5
+
+/** @class Herbivore
+  * Base class herbivore
+  */
 class Herbivore{
 public:
+	/** @brief Constructor
+	  * @param weight
+	  * Konstruktor herbivore dengan parameter berat badan 
+	  */
 	Herbivore(float weight);
-	static int nHerbivore;
+	/** Jumlah hewan herbivora */
+	static int nHerbivore; 
+	/** Jumlah total makanan herbivora */
 	static int total_hFood;
+	/** @brief Method getFood mengembalikan makanan herbivora
+	  */
 	int getFood() const;
 protected :
+	/** Makanan herbivora */
 	float hFood;
 };
 
-/** @class Carnivore
-  * Kelas yang merepresentasikan hewan karnivora.
+/** @class Carnovire
+  * Base class carnivore
   */
 class Carnivore{
 public:
-	/** @brief Constructor.
-	  * Membuat objek hewan karnivora dan mengincrement atribut nCarnivore
+	/** @brief Constructor
+	  * @param weight
+	  * Konstruktor carnivore dengan parameter berat badan 
 	  */
 	Carnivore(float weight);
+	/** @brief Method getFood mengembalikan makanan karnivora
+	  */
 	int getFood() const;
-	static int nCarnivore; 	/** Jumlah hewan karnivora */
+	/** Jumlah hewan karnivora */
+	static int nCarnivore; 	
+	/** Jumlah total makanan karnivora */
 	static int total_cFood;
 protected:
+	/** Makanan karnivora */
 	float cFood;
 };
 
+/** @class Omnivore
+  * Base class omnivore
+  */
 class Omnivore : public Herbivore, public Carnivore {
 public:
+	/** @brief Constructor
+	  * @param weight
+	  * Konstruktor omnivore dengan parameter berat badan 
+	  */
 	Omnivore(float weight);
+	/** @brief Method getFood mengembalikan makanan omnivora
+	  */
 	int getFood() const;
-	static int nOmnivore;
+	/** Jumlah hewan omnivora */
+	static int nOmnivore; 
+	/** Jumlah total makanan omnivora */
 	static int total_oFood;
 protected :
+	/** Makanan omnivora */
 	float oFood;
 };
 
