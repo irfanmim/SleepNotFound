530822ba204ed7daa6d931cbadd989e9f1557235
MIM <radioirfan@gmail.com>
2017-03-13 23:08:12 +0700

    Edit file zoo.cpp, driver.cpp dan buat Makefile

--- V1/driver.cpp
+++ V1/driver.cpp
@@ -13,16 +13,44 @@ Driver::Driver(ifstream& infile):z(infile){
 void Driver::DisplayMenu(){
 	do {
 		system("clear");
-		ShowZoo();
+
+		cout << "WELCOME TO VIRTUAL ZOO" << endl << endl;
+
 		for (int i = 0; i < menu.size(); i++) {
-			cout << menu[i] << endl;
+			if (i != menu.size() - 1) {
+				cout << menu[i] << endl;
+			}
+			else {
+				cout << menu[i];
+			}
 		}
 		cin >> inp;
-		if (inp == 2) {
-			quit = true;
-		}else if(inp == 1){
-			TourZoo();
+		
+		if (inp == 1) {
+			ShowZoo();
+			cout << endl << "Press any key to continue...";
+			cin.ignore(10,'\n');
+			cin.get();
 		}
+		else if (inp == 2) {
+			TourZoo();
+			cout << endl << "Press any key to continue...";
+			cin.ignore(10,'\n');
+			cin.get();
+		}
+		else if (inp == 3) {
+			quit = true;
+		}
+/*
+
+		if (inp == 3) {
+			quit = true;
+		}else if(inp == 2){
+			TourZoo();
+		}else if(inp == 1){
+			ShowZoo();
+		}
+*/
 	} while(!quit);
 }
 
@@ -36,8 +64,9 @@ void Driver::TourZoo(){
 
 void Driver::InitMenu(){
   menu.push_back("Pilihan menu : ");
-  menu.push_back("1. Tour ");
-  menu.push_back("2. Quit ");
+  menu.push_back("1. Display Virtual Zoo");
+  menu.push_back("2. Tour Virtual Zoo ");
+  menu.push_back("3. Quit \n");
   menu.push_back("Masukkan input : ");
 }
 
