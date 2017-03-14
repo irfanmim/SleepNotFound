e03c1b380f357adf70bc35a2fddcb8e4604ab56c
MIM <radioirfan@gmail.com>
2017-03-15 04:27:15 +0700

    komentar land_water_animal v3

--- VZ03 Formatted/land_water_animal/land_water_animal.h
+++ VZ03 Formatted/land_water_animal/land_water_animal.h
@@ -2,8 +2,16 @@
 #define LAND_WATER_ANIMAL_H
 #include "../animal/animal.h"
 
+/** @class LandWaterAnimal
+  * Kelas untuk binatang yang dapat hidup di antara dua habitat
+  */
 class LandWaterAnimal : public Animal{
 public:
+  /** @brief Constructor berparameter
+    * @param x Koordinat x
+    * @param y Koordinat y
+    * @param w Berat animal
+    */
   LandWaterAnimal(int x,int y,int w);
 };
 
